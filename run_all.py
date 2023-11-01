40baf992e1946932a465c65aff4392b818b349eb
jacobdenobel <jacobdenobel@gmail.com>
2023-11-01 03:46:37 +0100

    rung

--- run_all.py
+++ run_all.py
@@ -5,8 +5,8 @@ from main import STRATEGIES, ENVS
 
 SEEDS = range(1, 31)
 
-for env in ["BipedalWalker-v3"]:
-    for strat in ["ars-v1"]:
+for env in ["LunarLander-v2", "BipedalWalker-v3"]:
+    for strat in ["egs"]:
         for seed in SEEDS:
             subprocess.Popen([
                 "python",
@@ -20,8 +20,6 @@ for env in ["BipedalWalker-v3"]:
                 "--eval_total_timesteps",
                 "--sigma0",
                 "0.02",
-                "--eta",
-                "0.1"
             ], start_new_session=True)
             time.sleep(1)
 
