8396695dcadaa2a2adce587fa03f0af0b7f1bfbe
jacobdenobel <jacobdenobel@gmail.com>
2024-07-22 10:07:11 +0200

    fix typo

--- src/rl_es/objective.py
+++ src/rl_es/objective.py
@@ -115,10 +115,10 @@ class Objective:
     def __call__(self, x):
         
         if self.parallel:
-            f = np.r_[
+            f = np.r_[[
                 self.eval_parallel(split)
                 for split in np.array_split(x, np.ciel(x.shape[0] / self.max_parallel), axis=1)
-            ]
+            ]]
         else:
             f = np.array([self.eval_sequential(xi) for xi in x.T])
 
